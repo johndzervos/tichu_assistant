5c79577352c6dfb77b5d589eb750eb7259e53ebd
johndzervos <johndzervos@gmail.com>
2015-03-27 01:00:04 +0100

    updated2

--- detect2.py
+++ detect2.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img_rgb = cv2.imread('mainscene3.png')
+img_rgb = cv2.imread('scenes/mainscene9.png')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 threshold = 0.85
+
+dets = {}
 
 template = cv2.imread('cards2/mahjong.png',0)
 w, h = template.shape[::-1]
@@ -13,6 +15,7 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Mahjong detected"
+    dets['Mahjong'] = pt[0]
 
 template = cv2.imread('cards2/dragon.png',0)
 w, h = template.shape[::-1]
@@ -21,6 +24,7 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Dragon detected"
+    dets['Dragon'] = pt[0]
 
 template = cv2.imread('cards2/phoenix.png',0)
 w, h = template.shape[::-1]
@@ -28,7 +32,8 @@ res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    print "Phoenix detected" 
+    print "Phoenix detected"
+    dets['Phoenix'] = pt[0] 
 
 template = cv2.imread('cards2/dogs.png',0)
 w, h = template.shape[::-1]
@@ -36,15 +41,10 @@ res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    print "Dogs detected"       
+    print "Dogs detected"  
+    dets['Dogs'] = pt[0]     
 
-template = cv2.imread('cards2/red2.png',0)
-w, h = template.shape[::-1]
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    print "Red 2 detected"
+#################################### GREEN
 
 template = cv2.imread('cards2/green2.png',0)
 w, h = template.shape[::-1]
@@ -53,6 +53,16 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Green 2 detected"
+    dets['Green2'] = pt[0]
+
+template = cv2.imread('cards2/green3.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Green 3 detected"
+    dets['Green3'] = pt[0]
 
 template = cv2.imread('cards2/green4.png',0)
 w, h = template.shape[::-1]
@@ -61,6 +71,7 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Green 4 detected"
+    dets['Green4'] = pt[0]
 
 template = cv2.imread('cards2/green5.png',0)
 w, h = template.shape[::-1]
@@ -69,6 +80,7 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Green 5 detected"
+    dets['Green5'] = pt[0]
 
 template = cv2.imread('cards2/green6.png',0)
 w, h = template.shape[::-1]
@@ -77,6 +89,7 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Green 6 detected"
+    dets['Green6'] = pt[0]
 
 template = cv2.imread('cards2/green7.png',0)
 w, h = template.shape[::-1]
@@ -85,6 +98,7 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Green 7 detected"
+    dets['Green7'] = pt[0]
 
 template = cv2.imread('cards2/green8.png',0)
 w, h = template.shape[::-1]
@@ -93,6 +107,7 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Green 8 detected"
+    dets['Green8'] = pt[0]
 
 template = cv2.imread('cards2/green9.png',0)
 w, h = template.shape[::-1]
@@ -101,6 +116,7 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Green 9 detected"
+    dets['Green9'] = pt[0]
 
 template = cv2.imread('cards2/green10.png',0)
 w, h = template.shape[::-1]
@@ -109,22 +125,135 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Green 10 detected"
+    dets['Green10'] = pt[0]
 
-template = cv2.imread('cards2/blue2.png',0)
+template = cv2.imread('cards2/greenJ.png',0)
 w, h = template.shape[::-1]
 res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    print "Blue 2 detected"
+    print "Green J detected"
+    dets['GreenJ'] = pt[0]
 
-template = cv2.imread('cards2/black2.png',0)
+template = cv2.imread('cards2/greenQ.png',0)
 w, h = template.shape[::-1]
 res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    print "Black 2 detected"    
+    print "Green Q detected"
+    dets['GreenQ'] = pt[0]
+
+template = cv2.imread('cards2/greenK.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Green K detected"
+    dets['GreenK'] = pt[0]
+
+template = cv2.imread('cards2/greenA.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Green A detected"
+    dets['GreenA'] = pt[0]
+
+#################################### RED
+
+template = cv2.imread('cards2/red2.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red 2 detected"
+    dets['Red2'] = pt[0]
+
+template = cv2.imread('cards2/red3.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red 3 detected"
+    dets['Red3'] = pt[0]
+
+template = cv2.imread('cards2/red4.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red 4 detected"
+    dets['Red4'] = pt[0]
+
+template = cv2.imread('cards2/red5.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red 5 detected"
+    dets['Red5'] = pt[0]
+
+template = cv2.imread('cards2/red6.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red 6 detected"
+    dets['Red6'] = pt[0]
+
+template = cv2.imread('cards2/red7.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red 7 detected"
+    dets['Red7'] = pt[0]
+
+template = cv2.imread('cards2/red8.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red 8 detected"
+    dets['Red8'] = pt[0]
+
+template = cv2.imread('cards2/red9.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red 9 detected"
+    dets['Red9'] = pt[0]
+
+template = cv2.imread('cards2/red10.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red 10 detected"
+    dets['Red10'] = pt[0]
+
+template = cv2.imread('cards2/redJ.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red J detected"
+    dets['RedJ'] = pt[0]
 
 template = cv2.imread('cards2/redQ.png',0)
 w, h = template.shape[::-1]
@@ -133,6 +262,254 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Red Q detected"
+    dets['RedQ'] = pt[0]
+
+template = cv2.imread('cards2/redK.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red K detected"
+    dets['RedK'] = pt[0]
+
+template = cv2.imread('cards2/redA.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Red A detected"
+    dets['RedA'] = pt[0]
+
+############################# BLUE
+
+template = cv2.imread('cards2/blue2.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue 2 detected"
+    dets['Blue2'] = pt[0]
+
+template = cv2.imread('cards2/blue3.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue 3 detected"
+    dets['Blue3'] = pt[0]
+
+template = cv2.imread('cards2/blue4.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue 4 detected"
+    dets['Blue4'] = pt[0]
+
+template = cv2.imread('cards2/blue5.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue 5 detected"
+    dets['Blue5'] = pt[0]
+
+template = cv2.imread('cards2/blue6.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue 6 detected"
+    dets['Blue6'] = pt[0]
+
+template = cv2.imread('cards2/blue7.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue 7 detected"
+    dets['Blue7'] = pt[0]
+
+template = cv2.imread('cards2/blue8.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue 8 detected"
+    dets['Blue8'] = pt[0]
+
+template = cv2.imread('cards2/blue9.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue 9 detected"
+    dets['Blue9'] = pt[0]
+
+template = cv2.imread('cards2/blue10.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue 10 detected"
+    dets['Blue10'] = pt[0]
+
+template = cv2.imread('cards2/blueJ.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue J detected"
+    dets['BlueJ'] = pt[0]
+
+template = cv2.imread('cards2/blueQ.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue Q detected"
+    dets['BlueQ'] = pt[0]
+
+template = cv2.imread('cards2/blueK.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue K detected"
+    dets['BlueK'] = pt[0]
+
+template = cv2.imread('cards2/blueA.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Blue A detected"
+    dets['BlueA'] = pt[0]
+
+############################# BLACK
+
+template = cv2.imread('cards2/black2.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black 2 detected" 
+    dets['Black2'] = pt[0]   
+
+template = cv2.imread('cards2/black3.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black 3 detected"
+    dets['Black3'] = pt[0] 
+
+template = cv2.imread('cards2/black4.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black 4 detected"
+    dets['Black4'] = pt[0] 
+
+template = cv2.imread('cards2/black5.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black 5 detected"
+    dets['Black5'] = pt[0] 
+
+template = cv2.imread('cards2/black6.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black 6 detected"
+    dets['Black6'] = pt[0] 
+
+template = cv2.imread('cards2/black7.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black 7 detected"
+    dets['Black7'] = pt[0] 
+
+template = cv2.imread('cards2/black8.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black 8 detected"
+    dets['Black8'] = pt[0] 
+
+template = cv2.imread('cards2/black9.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black 9 detected"
+    dets['Black9'] = pt[0] 
+
+template = cv2.imread('cards2/black10.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black 10 detected"
+    dets['Black10'] = pt[0] 
+
+template = cv2.imread('cards2/blackJ.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black J detected"
+    dets['BlackJ'] = pt[0] 
+
+template = cv2.imread('cards2/blackQ.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black Q detected"
+    dets['BlackQ'] = pt[0] 
+
+template = cv2.imread('cards2/blackK.png',0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    print "Black K detected"
+    dets['BlackK'] = pt[0] 
 
 template = cv2.imread('cards2/blackA.png',0)
 w, h = template.shape[::-1]
@@ -141,5 +518,11 @@ loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     print "Black A detected"
+    dets['BlackA'] = pt[0] 
+
+#############################
+#print detections from left ro right, based on the x-coordinates
+print sorted(dets, key=dets.__getitem__)
+#print sorted(dets)
 
 cv2.imwrite('res.png',img_rgb) 
